d5981b4d31993a4a1cf489000bb18a772dda973a
hsw28 <holdbackaglacier@yahoo.com>
2023-09-14 14:28:43 -0500

    cries more

--- eyeblink/isomap_eigen.m
+++ eyeblink/isomap_eigen.m
@@ -1,26 +1,24 @@
-function [eigenvalues W D_degree] = isomap_eigen(Distance_matrix, k)
+function eigenvalues = isomap_eigen(Distance_matrix, k)
   %where D is the distance matrix
   % K =  k-nearest neighbors.
   % For example, for k-nearest neighbors with k = 5:
 
-D = Distance_matrix;
+Distance_matrix;
 
-% Step 1: Construct the Graph Laplacian matrix
-n = size(D, 1); % Number of data points
-W = zeros(n, n); % Initialize the adjacency matrix
+% Step 1: Construct the k-nearest neighbor graph
+   n = size(Distance_matrix, 1);
+   [sorted_distances, indices] = sort(Distance_matrix, 2); % Sort distances
+   k_neighbors = indices(:, 2:(k+1)); % Get the indices of k-nearest neighbors
 
+   % Step 2: Construct the adjacency matrix W
+   W = zeros(n, n);
+   for i = 1:n
+       W(i, k_neighbors(i, :)) = 1; % Assign 1 to the k-nearest neighbors
+   end
 
-for i = 1:n
-    % Find the k-nearest neighbors for each data point
-    [~, indices] = sort(D(i, :));
-    W(i, indices(1:k)) = 1; % Assign 1 to the k-nearest neighbors
-end
+   % Step 3: Compute the Laplacian matrix L
+   D = diag(sum(W, 2)); % Degree matrix
+   L = D - W;
 
-% Compute the degree matrix D_degree
-D_degree = diag(sum(W, 2));
-
-% Compute the unnormalized Laplacian matrix L
-L = D_degree - W;
-
-% Step 2: Compute the eigenvalues of the Laplacian matrix
-eigenvalues = eig(L);
+   % Step 4: Compute the eigenvalues of the Laplacian matrix L
+   eigenvalues = eig(L);
