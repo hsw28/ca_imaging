e3ae99e7ab6e1e3e4b8f7cb64f9d94dcdb51f83d
hsw28 <holdbackaglacier@yahoo.com>
2023-10-06 11:12:28 -0500

    slurl lyfe for lyfe

--- dim_reduction/slurm_scripts/B24B25_25.sh
+++ dim_reduction/slurm_scripts/B24B25_25.sh
@@ -2,9 +2,9 @@
 #SBATCH --account=p32072 ## YOUR ACCOUNT pXXXX or bXXXX
 #SBATCH --partition=normal ### PARTITION (buyin, short, normal, w10001, etc)
 #SBATCH --nodes=1 ## Never need to change this
-#SBATCH --ntasks-per-node=64 ##
-#SBATCH --time=20:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
-#SBATCH --mem=130G ## how much RAM you need per computer (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --ntasks-per-node=32 ##
+#SBATCH --time=12:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --mem=78G ## how much RAM you need per computer (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --output=matlab_job_result_B24B25_25.out ## standard out and standard error goes to this file
 
 
