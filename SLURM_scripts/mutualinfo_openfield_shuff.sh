6e2f9a490cfeef3d6f0203de90802e7f1684300a
hsw28 <holdbackaglacier@yahoo.com>
2024-01-09 11:39:08 -0600

    matlab on slurm is wtf

--- SLURM_scripts/mutualinfo_openfield_shuff.sh
+++ SLURM_scripts/mutualinfo_openfield_shuff.sh
@@ -1,19 +1,2 @@
-#!/bin/bash
-#SBATCH --account=p32072
-#SBATCH --partition=normal
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --mem=64GB
-#SBATCH --time=24:00:00
-#SBATCH --job-name="sample_job_\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
-#SBATCH --output=MI_shuff.out ## use the jobid (A) and the specific job index (a) to name your log file
-#SBATCH --mail-type=ALL ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (completed, failed, etc)
-#SBATCH --mail-user=hsw@northwestern.edu  ## your email
-#SBATCH --cpus-per-task=8
-
-# Load MATLAB module
 module load matlab/r2018b
-
-# Run MATLAB script
-
-matlab -nodisplay -nosplash -nodesktop -r "addpath(genpath('/home/hsw967/Programming/ca_imaging')); addpath(genpath('/home/hsw967/Programming/data_analysis/hannah-in-use/matlab/')); mutualinfo_openfield_shuff_4SLURM; exit;"
+matlab -singleCompThread -batch mutualinfo_openfield_shuff_4SLURM
--- SLURM_scripts/mutualinfo_openfield_shuff_4SLURM.m
+++ SLURM_scripts/mutualinfo_openfield_shuff_4SLURM.m
@@ -1,11 +1,55 @@
+% Get a handle to the cluster
+c = parcluster;
+
+%% Required arguments in order to submit a MATLAB GPU job
+% Specify the walltime (e.g. 4 hours)
+c.AdditionalProperties.WallTime = '024:00:00';
+% Specify an account to use for MATLAB jobs (e.g. pXXXX, bXXXX, etc)
+c.AdditionalProperties.AccountName = 'p32072';
+% Specify a queue/partition to use for MATLAB jobs (e.g. short, normal, long)
+c.AdditionalProperties.QueueName = 'normal';
+% Specify number of GPUs
+c.AdditionalProperties.GpusPerNode = 1;
+% Specify type of GPU card to use (e.g. a100)
+c.AdditionalProperties.GpuCard = 'a100';
+%% optional arguments but worth considering
+% Specify memory to use for MATLAB jobs, per core (default: 4gb)
+c.AdditionalProperties.MemUsage = '64gb';
+% Specify number of nodes to use
+c.AdditionalProperties.Nodes = 1;
+% Specify e-mail address to receive notifications about your job
+c.AdditionalProperties.EmailAddress = 'hsw967@northwestern.edu';
+
+% The script that you want to run through SLURM needs to be in the MATLAB PATH
+% Here we assume that quest_gpu_example.m lives in the same folder as submit_matlab_job.m
+addpath(pwd);
+addpath(genpath('/home/hsw967/Programming/ca_imaging'));
+addpath(genpath('/home/hsw967/Programming/data_analysis/hannah-in-use/matlab/'));
+
+
+% Finally we will submit the MATLAB script quest_gpu_example to SLURM such that MATLAB
+job = c.batch('mutualinfo_openfield_shuff_4SLURM', 'CurrentFolder', '.');
+
+
 function mutualinfo_openfield_shuff_4SLURM
 
-% Start a parallel pool with the specified number of workers
-numWorkers = 8; % This should match the number of CPUs requested in the SLURM script
-poolobj = gcp('nocreate'); % Check if the pool already exists
-if isempty(poolobj)
-    poolobj = parpool(numWorkers);
-end
+  % Refresh MATLAB's toolbox cache
+  rehash toolboxcache;
+
+  % Custom configuration for the cluster (if required)
+  % configCluster;
+
+  % Set the number of computational threads to the number of allocated CPUs
+  if ~isempty(getenv('SLURM_CPUS_PER_TASK'))
+      maxNumCompThreads(str2num(getenv('SLURM_CPUS_PER_TASK')));
+  end
+
+  % Start a parallel pool with the specified number of workers
+  numWorkers = 8;
+  poolobj = gcp('nocreate');
+  if isempty(poolobj)
+      poolobj = parpool(numWorkers);
+  end
 
 pos_structure = load('pos.mat');
 spikes = load('spikes.mat')
