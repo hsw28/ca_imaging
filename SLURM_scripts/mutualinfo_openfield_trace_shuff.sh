15dbe087cdcad3f23c0a9a5d18b08bfdfc9de469
hsw28 <holdbackaglacier@yahoo.com>
2024-01-09 12:13:19 -0600

    slirmmmmm

--- SLURM_scripts/mutualinfo_openfield_trace_shuff.sh
+++ SLURM_scripts/mutualinfo_openfield_trace_shuff.sh
@@ -1,2 +1,16 @@
-module load matlab/r2018b 
-matlab -batch SLURM_mutual_info_shuff
+#!/bin/bash
+#SBATCH --account=w10001 ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition=normal
+#SBATCH --nodes=1 ## Never need to change this
+#SBATCH --ntasks-per-node=1 ## Never need to change this
+#SBATCH --time=24:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --mem=64G ## how much RAM do you need per computer (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --job-name="shuff_trace\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
+#SBATCH --output=shuff_trace.%A_%a.out ## use the jobid (A) and the specific job index (a) to name your log file
+#SBATCH --mail-type=ALL ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (completed, failed, etc)
+#SBATCH --mail-user=hsw@northwestern.edu  ## your email
+
+
+module load matlab/r2018b
+
+matlab -batch -nodisplay -nosplash -nodesktop -r "addpath(genpath('/home/hsw967/Programming/ca_imaging')); addpath(genpath('/home/hsw967/Programming/data_analysis/hannah-in-use/matlab/')); mutualinfo_openfield_trace_shuff_4SLURM; exit;"
--- SLURM_scripts/mutualinfo_openfield_trace_shuff_4SLURM.m
+++ SLURM_scripts/mutualinfo_openfield_trace_shuff_4SLURM.m
@@ -1,30 +1,33 @@
-% Get a handle to the cluster
-c = parcluster;
 
-% Specify the walltime (e.g. 24 hours)
-c.AdditionalProperties.WallTime = '24:00:00';
-% Specify an account to use for MATLAB jobs (e.g. pXXXX, bXXXX, etc)
-c.AdditionalProperties.AccountName = 'p32072';
-% Specify a queue/partition to use for MATLAB jobs (e.g. short, normal, long)
-c.AdditionalProperties.QueueName = 'normal';
-% Specify memory to use for MATLAB jobs
-c.AdditionalProperties.MemUsage = '64gb';
-% Specify number of nodes to use
-c.AdditionalProperties.Nodes = 1;
-% Specify number of CPUs per node (optional, depending on your job requirements)
-c.AdditionalProperties.CpusPerNode = 8;
-% Specify e-mail address to receive notifications about your job
-c.AdditionalProperties.EmailAddress = 'hsw967@northwestern.edu';
+function mutualinfo_openfield_trace_shuff_4SLURM
+% Refresh MATLAB's toolbox cache
+rehash toolboxcache;
 
-rehash toolboxcache
-configCluster
+% Custom configuration for the cluster (if required)
+configCluster;
 
-% The script that you want to run through SLURM needs to be in the MATLAB PATH
-% Here we assume that quest_gpu_example.m lives in the same folder as submit_matlab_job.m
 addpath(pwd);
 addpath(genpath('/home/hsw967/Programming/ca_imaging'));
 addpath(genpath('/home/hsw967/Programming/data_analysis/hannah-in-use/matlab/'));
 
+% Set the number of computational threads to the number of allocated CPUs
+if ~isempty(getenv('SLURM_CPUS_PER_TASK'))
+    maxNumCompThreads(str2num(getenv('SLURM_CPUS_PER_TASK')));
+end
 
-% Finally we will submit the MATLAB script quest_gpu_example to SLURM such that MATLAB
-job = c.batch('trace_shuff_4SLURM', 'CurrentFolder', '.');
+% Start a parallel pool with the specified number of workers
+numWorkers = 8;
+poolobj = gcp('nocreate');
+if isempty(poolobj)
+    poolobj = parpool(numWorkers);
+end
+
+pos_structure = load('pos.mat');
+calcium_traces = load('traces.mat')
+ca_MI = load('MI_trace.mat')
+
+f = mutualinfo_openfield_trace_shuff(calcium_traces, pos_structure, 2, 2.5, 500, ca_MI)
+% Save the output to a .mat file
+MI_trace_shuff = f;
+save('mutualinfo_trace_shuff_output.mat', 'MI_trace_shuff');
+end
