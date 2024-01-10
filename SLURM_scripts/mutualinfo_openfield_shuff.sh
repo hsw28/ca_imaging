56f26d070d960d4d95f5535b8f700f7ab46b2f4b
hsw28 <holdbackaglacier@yahoo.com>
2024-01-10 17:20:01 -0600

    accidently said 1m instead of 1hr

--- SLURM_scripts/mutualinfo_openfield_shuff.sh
+++ SLURM_scripts/mutualinfo_openfield_shuff.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1 ## Never need to change this
 #SBATCH --ntasks-per-node=52 ## Never need to change this
 #SBATCH --cpus-per-task=1
-#SBATCH --time=00:1:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --time=03:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
 #SBATCH --mem=64G ## how much RAM do you need per computer (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name="shuff_\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
 #SBATCH --output=openfield_shuff.%A_%a.out ## use the jobid (A) and the specific job index (a) to name your log file
