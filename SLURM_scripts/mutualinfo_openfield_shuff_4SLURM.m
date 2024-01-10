0806d1d5d98a41e55e0fbf701e6ce8d3d10c28ea
hsw28 <holdbackaglacier@yahoo.com>
2024-01-10 16:29:32 -0600

    no idea

--- SLURM_scripts/mutualinfo_openfield_shuff_4SLURM.m
+++ SLURM_scripts/mutualinfo_openfield_shuff_4SLURM.m
@@ -1,11 +1,30 @@
 function mutualinfo_openfield_shuff_4SLURM
 
-  maxNumCompThreads(52);
-  cd ('/projects/p32072/data_eyeblink/rat314/slurm_variables')
-  outputFile = fopen('/projects/p32072/data_eyeblink/rat314/slurm_variables/mutualinfo_shuff_output.log', 'w');
-  try
+  fprintf('starting1')
+  maxNumCompThreads(str2num(getenv('SLURM_NPROCS')));
+fprintf('starting2')
+%{
+  % Refresh MATLAB's toolbox cache
+  rehash toolboxcache;
+
+  % Custom configuration for the cluster (if required)
+  configCluster;
 
 
+  c = parcluster;
+  c.AdditionalProperties.AccountName = 'p32072'; % Replace with your actual account name
+  c.AdditionalProperties.WallTime = '24:00:00'; % Set to match the wall time in your SLURM script
+  c.AdditionalProperties.QueueName = 'normal';
+  c.AdditionalProperties.MemUsage = '64gb';
+
+
+  % Set the number of computational threads to the number of allocated CPUs
+  if ~isempty(getenv('SLURM_CPUS_PER_TASK'))
+      maxNumCompThreads(str2num(getenv('SLURM_CPUS_PER_TASK')));
+  end
+%}
+
+fprintf('starting3')
     addpath(pwd);
     addpath(genpath('/home/hsw967/Programming/ca_imaging'));
     addpath(genpath('/home/hsw967/Programming/data_analysis/hannah-in-use/matlab/'));
@@ -20,28 +39,14 @@ function mutualinfo_openfield_shuff_4SLURM
   %peaks
   %pos
 
-parpool('p32072', 12)
-allvariables = load('/projects/p32072/data_eyeblink/rat314/slurm_variables/allvariables.mat');
+fprintf('starting4')
+allvariables = load('allvariables.mat');
 pos_structure = allvariables.pos;
 spikes = allvariables.peaks;
 ca_MI = allvariables.MI;
 
-f = mutualinfo_openfield_shuff(spikes, pos_structure, 2, 2.5, 5, ca_MI)
+f = mutualinfo_openfield_shuff(spikes, pos_structure, 2, 2.5, 500, ca_MI)
 MI_shuff = f;
 % Save the output to a .mat file
-
-
-try
-    save('./mutualinfo_shuff_output.mat', 'MI_shuff');
-catch e
-    disp('Error occurred while saving the file:');
-    disp(e.message);
-end
-
-
-fprintf(outputFile, 'Computation completed successfully.\n');
-catch e
-fprintf(outputFile, 'An error occurred: %s\n', e.message);
-end
- fclose(outputFile);
+save('mutualinfo_shuff_output.mat', 'MI_shuff');
 end
