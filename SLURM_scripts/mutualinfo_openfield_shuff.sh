feab77f45972c06e7f65dcb76d5107836f083503
hsw28 <holdbackaglacier@yahoo.com>
2024-01-10 13:59:59 -0600

    sl

--- SLURM_scripts/mutualinfo_openfield_shuff.sh
+++ SLURM_scripts/mutualinfo_openfield_shuff.sh
@@ -1,2 +1,2 @@
 module load matlab/r2018b
-matlab -singleCompThread -batch '/home/hsw967/Programming/ca_imaging/SLURM_scripts/submit_mutualinfo_openfield_shuff'
+matlab -singleCompThread -batch "addpath('/home/hsw967/Programming/ca_imaging/SLURM_scripts'); submit_mutualinfo_openfield_shuff"
