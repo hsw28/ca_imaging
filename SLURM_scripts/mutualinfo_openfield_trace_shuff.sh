1c57d2ecb39bf189f3a248bbd2f2b8cbcea170ed
hsw28 <holdbackaglacier@yahoo.com>
2024-01-09 22:14:10 -0600

    changing run length

--- SLURM_scripts/mutualinfo_openfield_trace_shuff.sh
+++ SLURM_scripts/mutualinfo_openfield_trace_shuff.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --account=p32072 ## YOUR ACCOUNT pXXXX or bXXXX
-#SBATCH --partition=normal
+#SBATCH --partition=long
 #SBATCH --nodes=1 ## Never need to change this
 #SBATCH --ntasks-per-node=8 ## Never need to change this
 #SBATCH --cpus-per-task=1
-#SBATCH --time=24:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --time=48:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
 #SBATCH --mem=64G ## how much RAM do you need per computer (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name="shuff_trace\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
 #SBATCH --output=shuff_trace.%A_%a.out ## use the jobid (A) and the specific job index (a) to name your log file
