2a926ec0877987fc650be0ab09e836d7d57d25e8
hsw28 <holdbackaglacier@yahoo.com>
2024-01-11 18:19:39 -0600

    changing pool size

--- SLURM_scripts/submit_mutualinfo_openfield_shuff.m
+++ SLURM_scripts/submit_mutualinfo_openfield_shuff.m
@@ -32,4 +32,4 @@ addpath(pwd)
 addpath(genpath('/home/hsw967/Programming/ca_imaging'))
 addpath(genpath('/home/hsw967/Programming/data_analysis/hannah-in-use/matlab/'));
 addpath(genpath('/home/hsw967/Programming/data_analysis/hannah-in-use/include'));
-job = c.batch('mutualinfo_openfield_shuff_4SLURM', 'Pool', 52, 'CurrentFolder', '.');
+job = c.batch('mutualinfo_openfield_shuff_4SLURM', 'Pool', 16, 'CurrentFolder', '.');
