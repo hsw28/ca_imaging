eead0b8c0263f6d71952bd8c689ecf290eba2895
hsw28 <holdbackaglacier@yahoo.com>
2024-01-16 16:31:29 -0600

    memory changes

--- SLURM_scripts/submit_mutualinfo_openfield_trace_shuff.m
+++ SLURM_scripts/submit_mutualinfo_openfield_trace_shuff.m
@@ -30,4 +30,4 @@ addpath(pwd)
 addpath(genpath('/home/hsw967/Programming/ca_imaging'))
 addpath(genpath('/home/hsw967/Programming/data_analysis/hannah-in-use/matlab/'));
 addpath(genpath('/home/hsw967/Programming/data_analysis/hannah-in-use/include'));
-job = c.batch('mutualinfo_openfield_trace_shuff_4SLURM', 'Pool', 9, 'CurrentFolder', '.');
+job = c.batch('mutualinfo_openfield_trace_shuff_4SLURM', 'Pool', 8, 'CurrentFolder', '.');
