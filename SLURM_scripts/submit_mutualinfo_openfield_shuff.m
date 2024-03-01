5a70a7e4ddfea52bc7c318db072d6cc0fc18c8bd
hsw28 <holdbackaglacier@yahoo.com>
2024-03-01 11:01:03 -0600

    processing updates

--- SLURM_scripts/submit_mutualinfo_openfield_shuff.m
+++ SLURM_scripts/submit_mutualinfo_openfield_shuff.m
@@ -16,7 +16,7 @@ c.AdditionalProperties.QueueName = 'normal';
 %% optional arguments but worth considering
 
 % Specify memory to use for MATLAB jobs, per core (default: 4gb)
-c.AdditionalProperties.MemUsage = '3gb';
+c.AdditionalProperties.MemUsage = '2gb';
 
 % Specify number of nodes to use
 c.AdditionalProperties.Nodes = 1;
@@ -37,4 +37,4 @@ addpath(genpath('/home/hsw967/Programming/ca_imaging'));
 addpath(genpath('/home/hsw967/Programming/data_analysis/hannah-in-use/matlab/'));
 addpath(genpath('/home/hsw967/Programming/data_analysis/hannah-in-use/include'));
 fprintf('starting the job');
-job = c.batch('mutualinfo_openfield_shuff_4SLURM', 'Pool', 10, 'CurrentFolder', '.');
+job = c.batch('mutualinfo_openfield_shuff_4SLURM', 'Pool', 12, 'CurrentFolder', '.');
