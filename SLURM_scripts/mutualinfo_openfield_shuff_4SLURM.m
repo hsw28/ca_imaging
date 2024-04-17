946d5eb83ba6d336f488439966642886cd546ba4
hsw28 <holdbackaglacier@yahoo.com>
2024-04-17 18:33:37 -0500

    for MI with 5cm bins

--- SLURM_scripts/mutualinfo_openfield_shuff_4SLURM.m
+++ SLURM_scripts/mutualinfo_openfield_shuff_4SLURM.m
@@ -24,14 +24,21 @@ fprintf('loading pos')
 pos_structure = slurm_var.pos;
 fprintf('loading spikes')
 spikes = slurm_var.Ca_peaks;
-fprintf('loading MI')
-ca_MI = slurm_var.MI;
 fprintf('loading TS')
 ca_ts = slurm_var.Ca_ts;
 fprintf('done loading')
 clearvars slurm_var
 fprintf('cleared excess variables, starting process')
 
-f = mutualinfo_openfield_shuff(spikes, pos_structure, 4, 2.5, ca_ts, 500, ca_MI);
+fprintf('loading MI')
+%for regular MI
+%ca_MI = slurm_var.MI;
+%for MI 5
+ca_MI = slurm_var.MI5;
+
+%for regular
+%f = mutualinfo_openfield_shuff(spikes, pos_structure, 4, 2.5, ca_ts, 500, ca_MI);
+%for MI5
+f = mutualinfo_openfield_shuff(spikes, pos_structure, 4, 5, ca_ts, 500, ca_MI);
 
 end
--- SLURM_scripts/mutualinfo_openfield_trace_shuff_4SLURM.m
+++ SLURM_scripts/mutualinfo_openfield_trace_shuff_4SLURM.m
@@ -22,16 +22,24 @@ fprintf('loading pos')
 pos_structure = slurm_var.pos;
 fprintf('loading traces')
 calcium_traces = slurm_var.Ca_traces;
-fprintf('loading MI')
-ca_MI = slurm_var.MI_trace;
 fprintf('loading times')
 ca_ts = slurm_var.Ca_ts;
 fprintf('clearing excess')
 clearvars slurm_var
 
-fprintf('starting script')
-f = mutualinfo_openfield_trace_shuff(calcium_traces, pos_structure, 4, 2.5, ca_ts, 500, ca_MI);
 
+fprintf('loading MI')
+%for regular MI
+%ca_MI = slurm_var.MI_trace;
+%for MI5
+ca_MI = slurm_var.MI_trace5;
+
+fprintf('starting script')
+%regular
+%f = mutualinfo_openfield_trace_shuff(calcium_traces, pos_structure, 4, 2.5, ca_ts, 500, ca_MI);
+
+%5
+f = mutualinfo_openfield_trace_shuff(calcium_traces, pos_structure, 4, 5, ca_ts, 500, ca_MI);
 
 
 
