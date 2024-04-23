3548ac680bd170c79b2e84a3d6b2721412876c0a
hsw28 <holdbackaglacier@yahoo.com>
2024-04-23 17:12:15 -0500

    typo lyfe

--- SLURM_scripts/mutualinfoCSUS_trace_shuff_4SLURM2_10.m
+++ SLURM_scripts/mutualinfoCSUS_trace_shuff_4SLURM2_10.m
@@ -29,12 +29,12 @@ fprintf('loading IDs')
 %for 10 divisions
 CSUS_id = slurm_var.CSUS_id10;
 ca_MI1 = slurm_var.MI_CSUS2_trace_10;
-ca_M2 = slurm_var.MI_CSUS2_trace_pt_10;
+ca_MI2 = slurm_var.MI_CSUS2_trace_pt_10;
 
 
 %%%%%BELOW IS FOR CSUS WITH 2 DIVISIONS NO PRETRIAL%%%%%%%%
 fprintf('loading MI')
-f = mutualinfo_CSUS_trace_shuff2(calcium_traces, CSUS_id, 0, 500, ca_MI1)
+%f = mutualinfo_CSUS_trace_shuff2(calcium_traces, CSUS_id, 0, 500, ca_MI1)
 
 
 f = mutualinfo_CSUS_trace_shuff2(calcium_traces, CSUS_id, 1, 500, ca_MI2)
